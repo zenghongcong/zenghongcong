eef067d36d1e8a6917521d5437dacb5c84e62907
zenghongcong <zenghongcong@baogongyoucai.com>
2019-02-15 18:35:52 +0800

    自动构建

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    name: 'p',
+    env: {
+      COMMON_VARIABLE: 'true'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '202.182.113.102',
+      ref  : 'origin/master',
+      repo : 'git@github.com:zenghongcong/zenghongcong.git',
+      path : '/var/www/zenghongcong',
+      'post-deploy' : 'npm install && npm run build'
+    }
+  }
+};
